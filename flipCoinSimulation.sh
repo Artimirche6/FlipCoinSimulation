b443c0204d2e1bf0d197e44ca9e0224eb7ca9013
Artimirche6 <artimirche6@gmail.com>
2021-09-12 00:09:46 +0530

    [arti] Added: As a Simulator start

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -1,3 +1,12 @@
 #! /bin/bash/ -x
 
-"This problem displays the winner Heads or Tails"
+echo "This problem displays the winner Heads or Tails"
+
+flip=$(($RANDOM%2))
+
+if [ $flip -eq 1 ]
+then
+	echo "HEAD"
+else
+	echo "TAIL"
+fi
