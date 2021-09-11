9741fac21b5816827c733275e6b6ace4120f6273
Artimirche6 <artimirche6@gmail.com>
2021-09-12 00:30:45 +0530

    [arti] Added: Winner Head or Tail

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -1,14 +1,12 @@
 #! /bin/bash/ -x
 
-read -p " Enter number of times the Coin is Flip : " num
+echo "This problem displays the winner Heads or Tails"
 
 head=0
 tail=0
 
-for((i=1; i<=$num; i++))
+while [ $head -le 21 ] && [ $tail -le 21 ]
 do
-	echo "This problem displays the winner Heads or Tails"
-
 	flip=$(($RANDOM%2))
 
 	if [ $flip -eq 1 ]
@@ -19,7 +17,27 @@ do
 		echo "TAIL"
 		tail=$(($tail+1))
 	fi
+
 done
 
 echo " Number of HEAD = " $head
 echo " Number of TAIL = " $tail
+
+
+if [ $head -eq $tail ]
+then
+	echo " Match Tie"
+else	
+	if [ $head -gt $tail ]
+	then
+		echo " HEAD WON "
+		win=$(($head-$tail))
+		echo " Head Won by $win Times"
+	else
+		echo " TAIL WON "
+                win=$(($tail-$head))
+                echo " Tail Won by $win Times"
+	fi
+fi
+
+
